4456fce900b42afc3935ac2251dd72eb80ed412a
nandish2484 <nandishp2484@gmail.com>
2025-04-27 11:24:45 +0530

    serverundo

--- server/server.js
+++ server/server.js
@@ -1,48 +1,48 @@
-import './config/instrument.js';
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import connectDB from './config/db.js';
-import * as Sentry from '@sentry/node';
-import { clerkWebhooks } from './controllers/webhooks.js';
-import companyRoutes from './routes/companyRoutes.js';
-import connnectCloudinary from './config/cloudinary.js';
-import jobRoutes from './routes/jobRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import { clerkMiddleware } from '@clerk/express';
+import './config/instrument.js'
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/db.js'
+import * as Sentry from "@sentry/node";
+import { clerkWebhooks } from './controllers/webhooks.js'
+import companyRoutes from './routes/companyRoutes.js'
+import connnectCloudinary from './config/cloudinary.js'
+import jobRoutes from './routes/jobRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import {clerkMiddleware} from '@clerk/express'
 
 // Initialize Express
-const app = express();
 
-// Connect database
-await connectDB();
-await connnectCloudinary();
+const app = express()
+
+//Connect database
+
+await connectDB()
+await connnectCloudinary()
 
 // Middlewares
-app.use(cors({
-    origin: 'https://job-portal-client-cwg97qcp2-nandish-panchals-projects.vercel.app', // Frontend URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-}));
-app.use(express.json({ limit: '10mb' })); // Increase payload size limit
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
-app.use(clerkMiddleware());
+
+app.use(cors())
+app.use(express.json())
+app.use(clerkMiddleware())
+
 
 // Routes
-app.get('/', (req, res) => res.send('API WORKING'));
-app.get('/debug-sentry', function mainHandler(req, res) {
-    throw new Error('Sentry error!');
-});
 
-app.post('/webhooks', clerkWebhooks);
-app.use('/api/company', companyRoutes);
-app.use('/api/jobs', jobRoutes);
-app.use('/api/users', userRoutes);
+app.get('/',(red,res)=>res.send("API WORKING"))
+app.get("/debug-sentry", function mainHandler(req, res) {
+    throw new Error("Sentry error!");
+  });
 
-const PORT = process.env.PORT || 5000;
+app.post('/webhooks',clerkWebhooks)
+app.use('/api/company',companyRoutes)
+app.use('/api/jobs',jobRoutes)
+app.use('/api/users',userRoutes)
+
+const PORT = process.env.PORT || 5000
 
 Sentry.setupExpressErrorHandler(app);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT} `);
+})
