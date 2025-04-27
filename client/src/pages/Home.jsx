b142d7598e8512281c29f0e1e39298dde94a1037
nandish2484 <nandishp2484@gmail.com>
2025-04-27 11:14:55 +0530

    footer

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import JobListing from '../components/JobListing'
-import Footer from '../components/footer'
+import Footer from '../components/Footer'
 
 
 const Home = () => {
